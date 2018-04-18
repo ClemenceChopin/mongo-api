c612a1006c0ef55ef97ee2d394ff9e8b16cd42e6
tely <tely@pc113.home>
2018-04-18 20:12:18 +0200

    initialisation mongo-api

---
+++ controllers/user-controller.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+
+    getUsers (req,res){
+        res.send({users:'des users'});
+    },
+    getUser (req,res){
+        res.send({user:'un user avec l\'id ' + req.params.id});
+    }
+}
