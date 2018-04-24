3d782c6611bb86301019e7904ef32c53ba552152
ClemenceChopin <telynor@gmail.com>
2018-04-24 11:42:24 +0200

    commentaire

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,12 @@ const MovieController = require('../controllers/movie-controller')
 
 module.exports = (server) => {
 
+    // users
     server.get('/users', UserController.readAll);
     server.get('/user/:id', UserController.read);
     server.post('/user', UserController.create);
     server.delete('/user/', UserController.delete);
-
+    // movies
     server.get('/movies', MovieController.readAll);
     server.get('/movie/:id', MovieController.read);
     server.post('/movie', MovieController.create);
